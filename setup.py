8d8496b5851b77af922457ccd70841984493b409
Vaixnav-017~ <vkvaishnav825@gmail.com~>
2026-02-22 17:58:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='ragnosis_medical_chatbot',
+    version='0.1.0',
+    author='Vaishnav V K',
+    packages=find_packages(),
+    install_requires=[]
+)
